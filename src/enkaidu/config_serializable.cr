6f64ae617078da928ba7a37889cbb22179015cab
nogginly <nogginly@icloud.com>
2025-08-19 00:08:53 -0400

    Support env var interpolation with default when missing

--- src/enkaidu/config_serializable.cr
+++ src/enkaidu/config_serializable.cr
@@ -8,10 +8,33 @@ module Enkaidu
     include YAML::Serializable
 
     private def gsub_with_env(value : String)
+      #
+      # Pretty nasty bunch of regex ... replace with something better
+      # one day.
+      #
+      # check if it has ${XXX:default} in it and replace with environment var
+      # of default value
+      tmp = value.gsub(/\$*\$\{[A-Za-z_][A-Za-z0-9_]*:-.+\}/) do |var_w_def|
+        if (ix = var_w_def.index('{')) && ix.odd?
+          # odd no. of '$', so keep even number (or none if 1), and interpolate
+          keep = ix > 1 ? var_w_def[..ix - 2] : ""
+          var_w_def = var_w_def[(ix + 1)..].rchop('}')
+          var, defval = var_w_def.split(":-", 2)
+          keep + (ENV[var]? || defval)
+        else
+          var_w_def # don't interpolate, even # of '$'
+        end
+      end
       # check if it has $XXX in it and replace with environment var
-      tmp = value.gsub(/\$[A-za-z-_]+/) do |var|
-        # look up without $ prefix
-        ENV[var.lchop('$')]
+      tmp = tmp.gsub(/\$*\$[A-za-z_][A-za-z0-9_]*/) do |var|
+        # Find last $ in sequence of odd $'s
+        if (ix = var.index(/\$[^\$]/)) && !ix.odd?
+          # odd no. of '$', so keep even number (or none if 1), and interpolate
+          keep = ix.zero? ? "" : var[..ix - 1]
+          keep + (ENV[var[(ix + 1)..]]? || var)
+        else
+          var # don't interpolate, even # of '$'
+        end
       end
       tmp
     end
