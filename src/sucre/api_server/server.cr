24650ed55b74d5ef2c69ba41c0187dab5563508e
nogginly <nogginly@icloud.com>
2025-09-04 00:11:46 -0400

    Make the server routable

--- src/sucre/api_server/server.cr
+++ src/sucre/api_server/server.cr
@@ -1,7 +1,8 @@
 require "http/server"
+require "json"
 
 class Server
-  enum Tracker
+  private enum Tracker
     StartHandler
     EndHandler
     EndServer
@@ -10,9 +11,11 @@ class Server
   getter port : Int32
   getter address : Socket::IPAddress
 
+  private alias HandlerProc = Proc(HTTP::Request, HTTP::Server::Response, Nil)
+
+  private getter routers = {} of String => HandlerProc
   private getter work_tracker = Channel(Tracker).new
   private getter work_finished = Channel(Bool).new
-
   private getter active_handlers = 0
 
   def initialize(@port)
@@ -44,28 +47,53 @@ class Server
     end
   end
 
+  private getter before_handler : HandlerProc?
+
+  def before_all(&block : HandlerProc)
+    @before_handler = block
+  end
+
+  def get(path, &handler : HTTP::Request, HTTP::Server::Response -> Nil)
+    routers["GET #{path}"] = handler
+  end
+
+  def post(path, &handler : HTTP::Request, HTTP::Server::Response -> Nil)
+    routers["POST #{path}"] = handler
+  end
+
   private def handle(context)
     work_tracker.send(Tracker::StartHandler)
-    puts "~~~ request: #{context.request.inspect}"
-    context.response.content_type = "text/plain"
-    context.response.print "Hello world!"
-    if context.request.path == "/quit"
-      @server.close
+    route = "#{context.request.method} #{context.request.path}"
+    if handler = routers[route]?
+      begin
+        if pre_handler = before_handler
+          pre_handler.call(context.request, context.response)
+        end
+        handler.call(context.request, context.response)
+      rescue err
+        STDERR.puts route
+        STDERR.puts err.inspect_with_backtrace
+        context.response.content_type = "application/json"
+        context.response.status_code = 500
+        context.response.print <<-ERROR
+        { "type" : "error", "message" : "#{err.to_s}" }
+        ERROR
+      end
     end
   ensure
     work_tracker.send(Tracker::EndHandler)
   end
 
-  def run
+  def close
+    @server.close
+  end
+
+  def start
     start_tracking
     start_listening
   end
 
-  def wait
+  def join
     work_finished.receive?
   end
 end
-
-s = Server.new(8080)
-s.run
-puts s.wait.inspect
