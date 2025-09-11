5e17b3b1ef6cb9d5e497202c50ddcb7cd0d9840f
Kumanan <kumanan.yogaratnam@rcgtconsulting.com>
2025-09-11 12:07:24 -0400

    Show more info when trace gets unexpected response

--- src/enkaidu/cli/trace_http.cr
+++ src/enkaidu/cli/trace_http.cr
@@ -11,16 +11,21 @@ class HTTP::Client
 
   def_around_exec do |request|
     if @@trace_http
-      STDERR.puts "--> HTTP request #{".." * @@count} #{request.method} #{request.hostname} #{request.path}".colorize(:cyan)
+      STDERR.puts "--> HTTP request #{".." * @@count} #{request.method} #{request.path}".colorize(:cyan)
       @@count += 1
     end
     response = yield
     if @@trace_http
       @@count -= 1
       if response.is_a? HTTP::Client::Response
-        STDERR.puts "<-- HTTP response #{".." * @@count} #{response.status_code} #{response.status} #{response.content_type}".colorize(:cyan)
+        STDERR.puts "<-- HTTP response #{".." * @@count} #{request.method} #{request.headers["Host"]?} #{request.path} #{response.status_code} #{response.status} #{response.content_type}".colorize(:cyan)
       else
         STDERR.puts "<-- HTTP response #{".." * @@count} #{response.inspect}".colorize(:cyan)
+        begin
+          raise Exception.new("Response is not HTTP::Client::Response")
+        rescue ex
+          STDERR.puts ex.inspect_with_backtrace
+        end
       end
     end
     response
