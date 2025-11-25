f12ebf803d474f8b3fa1e260d9c2963cf076c894
nogginly <nogginly@icloud.com>
2025-11-25 00:42:55 -0500

    Define `Env` and `Profile` and `.enkaidu/` profile folder with vars and prompts

---
+++ src/enkaidu/env.cr
@@ -0,0 +1,14 @@
+require "./env/*"
+
+module Enkaidu::Env
+  private macro define_sys_props
+    {% target_triple = Crystal::TARGET_TRIPLE.split('-') %}
+    SYSTEM_PROPERTIES = Profile::Variables{
+      "os_arch" => {{ target_triple.first }},
+      "os_name" => {{ target_triple.last }},
+    }
+  end
+
+  # Hash of system properties
+  define_sys_props
+end
