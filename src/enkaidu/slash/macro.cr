5a2f4d6f19432dee7bcab9ae99d745889debd657
nogginly <nogginly@icloud.com>
2025-11-29 17:33:10 -0500

    Add `/macro` command

---
+++ src/enkaidu/slash/macro.cr
@@ -0,0 +1,33 @@
+require "./command"
+
+module Enkaidu::Slash
+  class MacroCommand < Command
+    NAME = "/macro"
+
+    HELP = <<-HELP1
+    `#{NAME} [<sub-command>]`
+    - `ls`
+      - List all available macros.
+      - Macros can be executed by using the `!` sigil before the name of the macro. E.g. `!test`
+    HELP1
+
+    def name : String
+      NAME
+    end
+
+    def help : String
+      HELP
+    end
+
+    def handle(session_manager : SessionManager, cmd : CommandParser)
+      session = session_manager.session
+      if cmd.expect?(NAME, "ls")
+        session.list_all_macros
+      else
+        session.renderer.warning_with(
+          "ERROR: Unknown or incomplete sub-command: #{cmd.arg_at? 0}",
+          help: HELP, markdown: true)
+      end
+    end
+  end
+end
