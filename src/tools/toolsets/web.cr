fd4fbb1aa50dbf2f2e31bd557901f0b088ed94ee
nogginly <nogginly@icloud.com>
2026-01-18 21:07:32 -0500

    Consolidate common constants and helpers

--- src/tools/toolsets/web.cr
+++ src/tools/toolsets/web.cr
@@ -3,8 +3,27 @@ require "./web/*"
 
 module Tools
   module Web
+    MAX_CONTENT_SIZE = 256*1024
+
+    TEXT_CTYPE_PREFIXES = [
+      "text/",
+    ]
+    TEXT_CTYPE_SUFFIXES = [
+      "/json",
+      "/xml",
+      "/yaml",
+      "/toml",
+    ]
+
+    def self.text?(content_type)
+      TEXT_CTYPE_PREFIXES.any? { |prefix| content_type.starts_with?(prefix) } ||
+        TEXT_CTYPE_SUFFIXES.any? { |suffix| content_type.ends_with?(suffix) }
+    end
+
+    # setup the toolset
     toolset = ToolSet.create("Web") do
-      hold HttpGetTextTool
+      hold HttpGetWebPageTool
+      hold HttpGetWebAsMarkdownTool
     end
     Tools.register(toolset)
   end
