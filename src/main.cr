9c095321d8702c90e25f74e625e1d462e0981d6e
nogginly <nogginly@icloud.com>
2026-05-31 19:13:15 -0400

    Setup terminal console mode using latest `termify`

--- src/main.cr
+++ src/main.cr
@@ -1,3 +1,5 @@
+require "termify"
+
 require "./enkaidu/cli/*"
 require "./enkaidu/console/*"
 require "./enkaidu/wui/main"
@@ -8,7 +10,15 @@ module Enkaidu
     private getter opts : CLI::Options
     private getter console : Console::Renderer
 
+    private def setup_console
+      # Setup terminal and put it in VT mode (for platforms that need it)
+      term = Termify.terminal
+      term.setup_console
+      at_exit { term.restore_console }
+    end
+
     def initialize
+      setup_console
       @console = Console::Renderer.new
       @opts = CLI::Options.new(console)
 
