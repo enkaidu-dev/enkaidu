c6b02693a1e006b215273ee2f0ae0e9d3d8db5b1
nogginly <nogginly@icloud.com>
2026-07-08 14:25:12 -0400

    Flush on every write to recorder IO

--- src/enkaidu/recorder.cr
+++ src/enkaidu/recorder.cr
@@ -1,9 +1,7 @@
 module Enkaidu
   # Recorder class handles recording in-app events and debug events for troubleshooting LLM comms
   class Recorder
-    FLUSH_AFTER = 10
     private getter rec_io : IO?
-    private getter count_since_flush = 0
 
     # Initializes a new Recorder instance with an optional IO object for recording.
     def initialize(@rec_io = nil); end
@@ -20,9 +18,6 @@ module Enkaidu
       return unless io = rec_io
 
       yield io
-      @count_since_flush += 1
-      return unless count_since_flush >= FLUSH_AFTER
-
       flush
     end
 
@@ -30,7 +25,6 @@ module Enkaidu
     def flush
       return unless io = rec_io
       io.flush
-      @count_since_flush = 0
     end
 
     # Closes the rec_io.
