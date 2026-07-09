a987a555076dfe00b7bf2f7a1cb93a1821948b62
nogginly <nogginly@icloud.com>
2026-07-09 18:14:42 -0400

    Add support for contitional continuation commands that start with `?`. Macros execution is now managed using a stack of query queues where eacn invocation pushes the current queue and starts a new one, in effect maintaining call frames. Conditional continuation commands allow a macro to "break" out of the current frame, in effect conditionally exiting to the calling "frame."

--- src/enkaidu/runtime.cr
+++ src/enkaidu/runtime.cr
@@ -1,4 +1,5 @@
 require "./slash_commander"
+require "./runtime/*"
 
 module Enkaidu
   class Error < Exception; end
@@ -15,6 +16,8 @@ module Enkaidu
     getter commander : Slash::Commander
     getter renderer : SessionRenderer
     getter options : SessionOptions
+
+    private getter conditional_helper = ConditionalCommandHelper.new
 
     def session
       session_manager.current.session
@@ -41,31 +44,94 @@ module Enkaidu
       session_manager.deploy_injected_functions(session)
     end
 
+    alias QueryQueue = Array(String)
+    alias QueryQueueStack = Array(QueryQueue)
+
+    #
+    # Macros used by the execution loop
+    #
+
+    # Handling code for `?...` commands
+    private macro _execute_conditional_command_
+      result = conditional_helper.handle_conditional_command(q)
+      case result[:continue]
+      when .break?
+        query_queue = [] of String # Empty the current queue
+        if msg = result[:message]
+          renderer.respond_with(msg)
+        end
+      when .abort?
+        query_queue = [] of String
+        query_queue_stack = [] of Array(String)
+        renderer.error_with(result[:message] || "ERROR: Aborting: Unknown reason.")
+      end # else `Yes` so continue
+    end
+
+    # Handling code for `!...` commands
+    private macro _execute_macro_command_
+      if mac_queries = session.find_and_prepare_macro(q)
+        # Push the current query queue on to the stack of queues
+        # iff it's not already empty
+        query_queue_stack << query_queue unless query_queue.empty?
+        # make the macro's queries the current queue
+        query_queue = mac_queries
+        in_macro = true
+        yield Event::Macro
+      else
+        renderer.error_with("ERROR: Aborting: Unknown macro: #{q}")
+        break
+      end
+    end
+
+    # Handling code for `/...` commands
+    private macro _execute_slash_command_
+      if commander.make_it_so(q) == :done
+        yield Event::Done
+      end
+      yield Event::SlashCommand
+    end
+
     def execute_query(query : String, &)
+      # Maintain a stack of query queues; when marcos are invoked,
+      # push current query queue onto stack and use the macro's queries as
+      # current quuery queue.
+      # Why? In effect the current query queue is the "frame" for the currently running macro.
+      # This should allow us to support, for e.g., a `?break_if CONDITION`
+      # command that aborts current macro and continue with the one that
+      # called it.
+      query_queue_stack = QueryQueueStack.new
+      # Current command or commands (if macro invoked)
       query_queue = [query]
+      # Track when macro is invoked
       in_macro = false
+
       while q = query_queue.shift?
         renderer.user_query_text(q, via_query_queue: true) if in_macro
 
         case q = q.strip
+        when .starts_with?("?")
+          _execute_conditional_command_
         when .starts_with?("!")
-          if mac_queries = session.find_and_prepare_macro(q)
-            # Expand the macro at the top of the queue, where
-            # next query awaits; essentially inserting the macro
-            query_queue.insert_all(0, mac_queries)
-          end
-          in_macro = true
-          yield Event::Macro
+          _execute_macro_command_
         when .starts_with?("/")
-          if commander.make_it_so(q) == :done
-            yield Event::Done
-          end
-          yield Event::SlashCommand
+          _execute_slash_command_
         else
           session.ask(query: q,
             attach: commander.take_inclusions!,
             response_json_schema: commander.take_response_schema!)
           yield Event::Prompt
+        end
+        # Check if query queue is empty, and pop from the query queue stack
+        # to continue with next query in outer frame
+        if query_queue.empty?
+          # It's possible prior queues in stack are empty, so we need to unwind until
+          # - either nothing is left
+          # - or we get to a queue with items
+          while !query_queue_stack.empty?
+            # Replace current query queue with one we pushed last onto stack of queues
+            query_queue = query_queue_stack.pop
+            break unless query_queue.empty?
+          end
         end
       end
     rescue ex
---
+++ src/enkaidu/runtime/conditional_command_helper.cr
@@ -0,0 +1,126 @@
+require "../../sucre/command_parser"
+require "../../tools"
+
+module Enkaidu
+  class ConditionalCommandHelper
+    include Tools::FileHelper
+
+    # Defines the next step following a conditional command
+    enum Continue
+      # Yes means continue with next command in queue
+      Yes
+      # Break means discard current "frame" queue, and continue with next "frame" in the stack
+      # Effectively terminates current macro
+      Break
+      # Abort means discard current frame queue and all queues in the stack, returning user to
+      # the prompt. Reserved for errors
+      Abort
+    end
+
+    # Conditional command handling returns a continuation response
+    # with a `Continue` value and an optional message.
+    alias Continuation = NamedTuple(continue: Continue, message: String?)
+
+    # Return a Tuple with
+    # - continuation with Abort and message if error, OR
+    # - resolved file path
+    private def resolve_and_validate(file : String) : Continuation | String
+      resolved = resolve_path(file)
+
+      unless within_current_directory?(resolved)
+        return {continue: Continue::Abort, message: "ERROR: File is outside current folder, not allowed."}
+      end
+
+      resolved
+    end
+
+    # Return a continuation after checking if  given file exists.
+    private def break_if_file_exists(file : String) : Continuation
+      case result = resolve_and_validate(file)
+      when Continuation then result
+      else
+        resolved_file = result
+        continue = Continue::Yes
+        message = nil.as(String?)
+        if file_exists?(resolved_file)
+          continue = Continue::Break
+          message = "INFO: Break: File exists: #{file}"
+        end
+        {continue: continue, message: message}
+      end
+    end
+
+    # Return a continuation after checking if given file does not exists.
+    private def break_unless_file_exists(file : String) : Continuation
+      case result = resolve_and_validate(file)
+      when Continuation then result
+      else
+        resolved_file = result
+        continue = Continue::Yes
+        message = nil.as(String?)
+        unless File.exists?(resolved_file)
+          continue = Continue::Break
+          message = "INFO: Break: File does not exist: #{file}"
+        end
+        {continue: continue, message: message}
+      end
+    end
+
+    # Return a continuation if given `file` exists AND `contains` the given string.
+    private def break_if_file_contains(file : String, contains : String) : Continuation
+      case result = resolve_and_validate(file)
+      when Continuation then result
+      else
+        resolved_file = result
+        continue = Continue::Yes
+        if File.exists?(resolved_file) && File.read(resolved_file).includes?(contains)
+          continue = Continue::Break
+          message = "INFO: Break: File contains \"#{contains}\""
+        end
+        {continue: continue, message: message}
+      end
+    end
+
+    # Return a continuation if given `file` exists AND `contains` the given string.
+    private def break_unless_file_contains(file : String, contains : String) : Continuation
+      case result = resolve_and_validate(file)
+      when Continuation then result
+      else
+        resolved_file = result
+        continue = Continue::Break
+        if File.exists?(resolved_file) && File.read(resolved_file).includes?(contains)
+          continue = Continue::Yes
+        else
+          message = "INFO: Break: File doesn't exist, or doesn't contain \"#{contains}\""
+        end
+        {continue: continue, message: message}
+      end
+    end
+
+    # Return true to continue with next query, or
+    # false to abort current query queue
+    def handle_conditional_command(q : String) : Continuation
+      case cmd = CommandParser.new(q)
+      when .expect?("?break_if", file_exists: String)
+        break_if_file_exists(
+          file: cmd.arg_named("file_exists").as(String))
+      when .expect?("?break_if", file: String, contains: String)
+        break_if_file_contains(
+          file: cmd.arg_named("file").as(String),
+          contains: cmd.arg_named("contains").as(String))
+      when .expect?("?break_unless", file_exists: String)
+        break_unless_file_exists(
+          file: cmd.arg_named("file_exists").as(String))
+      when .expect?("?break_unless", file: String, contains: String)
+        break_unless_file_contains(
+          file: cmd.arg_named("file").as(String),
+          contains: cmd.arg_named("contains").as(String))
+      else
+        {
+          continue: Continue::Abort,
+          message:  "ERROR: Aborting! Unknown conditional command: #{q}",
+        }
+      end
+    end
+  end
+end
