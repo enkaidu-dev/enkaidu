bba609b0f5c799a2600a30b80c1666ff7da7212a
nogginly <nogginly@icloud.com>
2026-08-31 01:35:04 -0400

    Support Ollama cloud-hosted models

--- src/llm/ollama/connection.cr
+++ src/llm/ollama/connection.cr
@@ -15,15 +15,19 @@ module LLM::Ollama
       super || ENV["OLLAMA_MODEL"]?
     end
 
+    def api_key : String?
+      ENV.fetch("OLLAMA_API_KEY", "ollama")
+    end
+
     protected def path : String
       "/v1/chat/completions"
     end
 
-    protected def headers : HTTP::Headers
-      HTTP::Headers{
-        "Content-Type"  => "application/json",
-        "Authorization" => "Bearer ollama",
-      }
-    end
+    # protected def headers : HTTP::Headers
+    #   HTTP::Headers{
+    #     "Content-Type"  => "application/json",
+    #     "Authorization" => "Bearer ollama",
+    #   }
+    # end
   end
 end
